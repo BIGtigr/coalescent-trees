0f5a571b6dc45611b99e0c14a8bcd454a6fc7717
Jahsh <joshua.espinas@gmail.com>
2018-05-11 12:29:21 +1200

    Converted all functions so far into a kingman class

--- coalescent_trees.py
+++ coalescent_trees.py
@@ -2,34 +2,56 @@ from tree import Node
 # from itertools import combinations
 import math
 
-def nCr(n, r):
-    f = math.factorial
-    return f(n) // (f(r) * f(n-r))
-
-def simulate_trees(n, pop_size):
-    """ Simulates trees according to the Kingman coalescent model.
-
-    inputs: number of leaves, n; effective population size, pop_size
-    output: coalescent tree with n leaves.
+class Kingman:
+    """ For all the operations to create a Kingman coalescent model
     """
+    def __init__(self):
+        self.leaves = []
 
-    # each pair of lineages coalesces at rate 1/pop_size
-    # given k lineages, total rate of coalescence is combination(k, 2)/pop_size
+    def simulate_trees(self, n, pop_size):
+        """ Simulates trees according to the Kingman coalescent model.
+    
+        inputs: number of leaves, n; effective population size, pop_size
+        output: coalescent tree with n leaves.
+        """
 
-    k = n   # setting lineages to n
-    t = 0
+        # each pair of lineages coalesces at rate 1/pop_size
+        # given k lineages, total rate of coalescence is combination(k, 2)/pop_size
 
-    leaves = []
-    for i in range(n):
-        leaves.append(Node(f'leaf {i}'))
+        k = n   # setting lineages to n
+        t = 0
 
-    for i in leaves:
-        print(i.get_label())
+        leaves = []
+        for i in range(n):
+            leaves.append(Node(f'leaf {i}'))
 
-    # while k > 1:
+        for i in leaves:
+            print(i.get_label())
+
+        while k > 1:
+            t_k = math.exp(self.ncr(k, 2)/pop_size)
+            t = t + t_k
+
+            m = Node()
+
+            print(t_k)
+            k -= 1
+
+    @staticmethod
+    def ncr(n, r):
+        """ nCr caclculation. n choose r. combinations with repetition
+
+        :param n: number of options
+        :param r: number selected from n
+        :return: number of combinations with repetition
+        """
+        f = math.factorial
+        return f(n) // (f(r) * f(n - r))
 
 
 def main():
-    simulate_trees(3, 10)
+    kingman = Kingman()
+    kingman.simulate_trees(3, 10)
+
 
 main()
