df18ca982173e575e2aaa59bc717443ee08946bd
Jahsh <joshua.espinas@gmail.com>
2018-05-11 13:11:05 +1200

    set children of node m to two random available leaves

--- coalescent_trees.py
+++ coalescent_trees.py
@@ -1,6 +1,7 @@
 from tree import Node
 # from itertools import combinations
 import math
+import random as ra
 
 class Kingman:
     """ For all the operations to create a Kingman coalescent model
@@ -28,13 +29,28 @@ class Kingman:
         for i in leaves:
             print(i.get_label())
 
-        while k > 1:
+        while k > 2:
             t_k = math.exp(self.ncr(k, 2)/pop_size)
             t = t + t_k
 
-            m = Node()
+            print(t)
 
-            print(t_k)
+            # new node m, with height t, and random children from available leaves.
+            m = Node()
+            m.set_height(t)
+
+            children = ra.sample(leaves, 2)
+            for node in children:
+                m.add_child(node)
+
+            for i in m.get_leaves():
+                print(i.get_label())
+            # for i in children:
+            #     print(i.get_label())
+
+
+
+            # print(t_k)
             k -= 1
 
     @staticmethod
@@ -52,6 +68,7 @@ class Kingman:
 def main():
     kingman = Kingman()
     kingman.simulate_trees(3, 10)
-
+    # x = ra.randint(0, 4)
+    # print(x)
 
 main()
